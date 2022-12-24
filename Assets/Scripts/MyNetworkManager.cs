8d47a464a6774f21757d4de95a8cdb6331fa2dcb
UnityLV <104660372+UnityLV@users.noreply.github.com>
2022-12-24 09:22:35 +0300

    Add Enviroment

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,33 @@
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        Debug.Log("Server Starting");
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+
+        Debug.Log("Server Stoping");
+    }
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+
+        Debug.Log("Connect to server");
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+
+        Debug.Log("Disconect to server");
+    }
+}
--- Assets/Scripts/PlayerCube.cs
+++ Assets/Scripts/PlayerCube.cs
@@ -1,14 +1,20 @@
 using Mirror;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCube : NetworkBehaviour
 {
+    [SyncVar(hook = nameof(OnHolaCountChanged))]
+    private int _holaCount;   
 
     private void Update()
     {
         Move();
+
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("Send hola to server");
+            Hola();
+        }
     }
     private void Move()
     {
@@ -20,4 +26,32 @@ public class PlayerCube : NetworkBehaviour
             transform.position += movement;
         }
     }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log("Starting server On Player");
+
+    }
+
+    [Command]
+    public void Hola()
+    {
+        Debug.Log(" Resived Hola from Client");
+        _holaCount++;
+        ReplyHola();
+    }
+
+    [TargetRpc]
+    private void ReplyHola()
+    {
+        Debug.Log("Resived Hola from Server");
+    }
+
+    private void OnHolaCountChanged(int oldValue,int newValue)
+    {
+        Debug.Log($"old count is {oldValue} and new is {newValue}");
+    }
+
+
 }
